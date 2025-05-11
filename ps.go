5dc444b8ef36c6ade304e51af6842e91516dec8b
Thuc Le <ledongthuc@gmail.com>
2025-05-11 13:17:57 +0700

    fix: https://github.com/ledongthuc/pdf/issues/42

--- ps.go
+++ ps.go
@@ -123,7 +123,9 @@ func Interpret(strm Value, do func(stk *Stack, op string)) {
 					val := stk.Pop()
 					key, ok := stk.Pop().data.(name)
 					if !ok {
-						panic("def of non-name")
+						// panic(fmt.Sprintf("def of non-name: %+v", stk.Pop().data))
+						// Skip the value if it has key without value
+						continue
 					}
 					dicts[len(dicts)-1][key] = val.data
 					continue
